759e37d556eb1bd677407d1121bef253bea65ca6
Harshith Pabbati <pabbatiharshith@gmail.com>
2020-07-16 00:35:42 +0530

    rename schemaForType to schemaAsFormat (#2)

--- src/components/OperationExplorer.tsx
+++ src/components/OperationExplorer.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import List, { ListRow } from "./common/List";
 import Toolbar from "./common/Toolbar";
-import { Tab } from "./common/Toolbar/Tabs";
+import { Tabs} from "./common/Toolbar/Tabs";
 import ThemeProvider from "./common/themes/provider";
 
 import Operation from "./Operation";
